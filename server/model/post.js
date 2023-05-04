1436e43ec84f7f5de42a9644258a88b4ff053428
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-04 16:16:34 +0100

    added basic necessary code, added getAll functionality

---
+++ server/model/post.js
@@ -0,0 +1,21 @@
+const db = require("../database/connect")
+class Post {
+    constructor({post_id, title, entry_date, content}) {
+        this.id = post_id
+        this.title = title
+        this.date = entry_date
+        this.content = content
+    }
+
+    static async getAll(){
+        const response = await db.query("SELECT * FROM post")
+        if (response.rows.length === 0){
+            throw new Error ("No diary entries have been made")
+        }
+
+        return response.rows.map(p => new Post(p))
+    }
+}
+
+
+module.exports = Post
