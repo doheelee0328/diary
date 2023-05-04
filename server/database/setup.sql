79aa538538f2ec77e09854b9af39d2fc2cb6fb58
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-04 16:37:50 +0100

    modify date entry format in setup.sql

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS post;
 CREATE TABLE post (
     post_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR (100) NOT NULL,
-    entry_date DATE DEFAULT CURRENT_DATE NOT NULL,
+    entry_date DATE NOT NULL,
     content VARCHAR (500) NOT NULL,
     PRIMARY KEY (post_id)
 );
 
 INSERT INTO post
-    (title,content)
+    (title,entry_date,content)
 VALUES
-    ('Test', 'Welcome to your first diary entry'),
-    ('Test', 'Filler post 1'),
-    ('Test', 'Filler post 2');
+    ('Test', '1995-07-30','Welcome to your first diary entry'),
+    ('Test','2001-03-15', 'Filler post 1'),
+    ('Test', '2023-01-20','Filler post 2');
