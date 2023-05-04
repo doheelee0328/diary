7014e0949b6577a3dadc75f4ad3732813fe4dccf
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-04 16:19:31 +0100

    added endpoints

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const postRouter = require('./router/post')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use("/entries", postRouter)
+
 app.get('/', (req, res) => {
-  res.json('hello')
+  res.json('Welcome to the Diary API :)')
 })
 
 module.exports = app
