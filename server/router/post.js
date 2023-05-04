112b322c04231d011a005eded441603b18cf603e
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-04 17:55:49 +0100

    added getById functionality

--- server/router/post.js
+++ server/router/post.js
@@ -5,5 +5,6 @@ const postRouter = Router()
 
 
 postRouter.get("/",postContoller.index)
+postRouter.get("/:id",postContoller.getIndexID)
 postRouter.post("/",postContoller.create)
 module.exports = postRouter
